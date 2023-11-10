f9aec2cb41fbd0e10bbb403fc1255ca5de715142
Makiasake <11155130+makiasake@users.noreply.github.com>
2023-11-10 15:28:12 -0300

    Update server.js
    added cors, Access-Control-Allow-Origin

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 
-const port = 3003
+const port = 3000
 const app = express()
+app.use(cors())
 
 app.get('/test', (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
     res.send({response: 'Ok!'})
 })
 
